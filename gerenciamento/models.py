60468d21fc0ca41373f955e51b1824613c5ac6bd
Philipe Macedo <philipemacedo23x@gmail.com>
2022-11-05 22:00:44 -0300

    Feat: add model do participante

--- gerenciamento/models.py
+++ gerenciamento/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Participante(models.Model):
+    primeiro_nome = models.CharField(max_length=255, null=True),
+    ultimo_nome = models.CharField(max_length=255, null=True)
+    matricula = models.CharField(max_length=255, null=True)
+    codigo = models.CharField(max_length=6, null=True)
+
+    def __str__(self):
+        return self.primeiro_nome + ' ' + self.ultimo_nome
+
+
+class Entrada(models.Model):
+    participante = models.ManyToManyField(Participante, on_delete=models.CASCADE)
+    horario = models.DateTimeField(auto_now=True)
+
+class Saida(models.Model):
+    participante = models.ManyToManyField(Participante, on_delete=models.CASCADE)
+    horario = models.DateTimeField(auto_now=True)
+    
